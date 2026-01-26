1d600248d9e53c967f4df5306f96a447c7cd04f9
legion2004 <milindabarua04@gmail.com>
2026-01-26 23:43:05 +0530

    updated something

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
-const PORT = 8083;
+const PORT = 9002;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname)));
