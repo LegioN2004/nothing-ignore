ca5841482319fd8b864e17c237641ef7fd36df43
legion2004 <milindabarua04@gmail.com>
2026-01-27 11:23:35 +0530

    updated the port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
-const PORT = 9002;
+const PORT = 9102;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname)));
